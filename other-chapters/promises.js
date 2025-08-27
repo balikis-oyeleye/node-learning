7837612839b701f99f026620796df6072b699bd5
Ryu <96819668+balikis-oyeleye@users.noreply.github.com>
2025-08-27 17:26:36 +0100

    delete folder

--- other-chapters/promises.js
+++
@@ -1,9 +0,0 @@
-const p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error("promises failed"));
-  }, 2000);
-});
-
-p.then((result) => {
-  console.log(result);
-}).catch((error) => console.log(error.message));
